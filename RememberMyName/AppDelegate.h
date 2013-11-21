6935b622071b118c3f198c6e1082e6593b73ca41
Daniel Baldwin <daniel.j.baldwin@hotmail.com>
2013-11-21 12:01:14 -0600

    first commit

---
+++ RememberMyName/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RememberMyName
+//
+//  Created by Daniel Baldwin on 11/21/13.
+//  Copyright (c) 2013 Dnlbldwn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RememberMyName/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RememberMyName
+//
+//  Created by Daniel Baldwin on 11/21/13.
+//  Copyright (c) 2013 Dnlbldwn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
